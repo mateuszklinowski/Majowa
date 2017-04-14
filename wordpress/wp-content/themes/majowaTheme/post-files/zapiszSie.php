5c029c97c4f056e213aac055dad54cb07e331e12
Mateusz Klinowski <klinekklinus@gmail.com>
2017-04-14 16:08:59 +0100

    this wont work

--- wordpress/wp-content/themes/majowaTheme/post-files/zapiszSie.php
+++ wordpress/wp-content/themes/majowaTheme/post-files/zapiszSie.php
@@ -2,25 +2,72 @@
 <!--Latest 4 posts-->
 <div>
 
+<?php
+if(isset($_POST['entry.1782511083'])){
+
+    $imie = $_POST['entry.1782511083'];
+    $nazwisko = $_POST['entry.173465573'];
+    $telefon = $_POST['entry.774965994'];
+    $email = $_POST['entry.1943066783'];
+    $radio = $_POST['entry.1994805656'];
+    $taniec = $_POST['entry.491094824'];
+
+    $form_id ='1HLmqkR4ULHnNvPxnjtUHdotzSLjp5UIIndsMk4FrqkE';
+
+    $link = 'https://docs.google.com/forms/d/'.$form_id.'/formResponse -d ifq -d "entry.1202908243=Hello World" -d submit=Submit';
 
 
-    <form>
-        <input type="text" placeholder="Imie">
-        <input type="text" placeholder="Nazwisko">
-        <input type="text" placeholder="Nr telefonu">
-        <input type="email" placeholder="Email">
-        <input type="radio" name="solo">
-        <input type="radio" name="solo">
-        <select>
-            <option>Tango</option>
-            <option>Jazz</option>
-            <option>Salsa</option>
-            <option>Zumba</option>
-            <option>Latino Solo</option>
-            <option>Pilates</option>
-            <option>Joga</option>
-            <option>Gimnastyka</option>
-            <option>Taniec użytkowy</option>
+
+}
+?>
+
+    <script>
+        !function(exports) {
+            exports.submitGoogleForm = submitGoogleForm;
+
+            function submitGoogleForm(form) {
+                try {
+                    var data = [].slice.call(form).map(function(control) {
+                        return 'value' in control && control.name ?
+                        control.name + '=' + (control.value === undefined ? '' : control.value) :
+                            '';
+                    }).join('&');
+                    var xhr = new XMLHttpRequest();
+
+                    xhr.open('POST', form.action + '/formResponse', true);
+                    xhr.setRequestHeader('Accept',
+                        'application/xml, text/xml, */*; q=0.01');
+                    xhr.setRequestHeader('Content-type',
+                        'application/x-www-form-urlencoded; charset=UTF-8');
+                    xhr.send(data);
+                } catch(e) {}
+
+                form.parentNode.className += ' submitted';
+
+                return false;
+            }
+        }(typeof module === 'undefined' ? window : module.exports);
+    </script>
+
+    <form method="POST"
+          action="https://docs.google.com/forms/d/1HLmqkR4ULHnNvPxnjtUHdotzSLjp5UIIndsMk4FrqkE"
+          onsubmit="return window.submitGoogleForm(this);">
+        <input type="text" placeholder="Imie" name="entry.1782511083">
+        <input type="text" placeholder="Nazwisko" name="entry.173465573">
+        <input type="text" placeholder="Nr telefonu" name="entry.774965994">
+        <input type="email" placeholder="Email" name="entry.1943066783">
+        <input type="radio" name="entry.1994805656" value="Solo">
+        <input type="radio" name="entry.1994805656" value="Z partnerem">
+        <select name="entry.491094824">
+            <option value="taniec">Tango</option>
+            <option value="taniec">Jazz</option>
+            <option value="taniec">Salsa</option>
+            <option value="taniec">Zumba</option>
+            <option value="taniec">Latino Solo</option>
+            <option value="taniec">Pilates</option>
+            <option value="taniec">Joga</option>
+            <option value="taniec">Gimnastyka</option>
+            <option value="taniec">Taniec użytkowy</option>
         </select>
         <input type="submit">
     </form>
